f414f6ace91533f2c04f25ba37657b0422dc4b5c
casper <connollynh@gmail.com>
2022-10-11 13:23:32 -0500

    update or set api

--- plugin/src/main/java/io/github/rysefoxx/pagination/InventoryContents.java
+++ plugin/src/main/java/io/github/rysefoxx/pagination/InventoryContents.java
@@ -113,6 +113,66 @@ public class InventoryContents {
     }
 
     /**
+     * Updates the ItemStack in the same place with a new ItemStack or sets if not found.
+     *
+     * @param slot      The slot
+     * @param itemStack The new ItemStack what should be displayed.
+     * @throws IllegalArgumentException if slot greater than 53 or slot greater than inventory size
+     */
+    public void updateOrSet(@Nonnegative Collection<Integer> slot,
+                            @NotNull ItemStack itemStack) throws IllegalArgumentException {
+
+        for (Integer integer : slot) {
+            updateOrSet(integer, itemStack);
+        }
+    }
+
+    /**
+     * Updates the ItemStack in the same place with a new ItemStack or sets if not found.
+     *
+     * @param slot      The slot
+     * @param intelligentItem The new ItemStack what should be displayed.
+     * @throws IllegalArgumentException if slot greater than 53 or slot greater than inventory size
+     */
+    public void updateOrSet(@Nonnegative Collection<Integer> slot,
+                            @NotNull IntelligentItem intelligentItem) throws IllegalArgumentException {
+        for (Integer integer : slot) {
+            updateOrSet(integer, intelligentItem);
+        }
+    }
+
+    /**
+     * Updates the ItemStack in the same place with a new ItemStack or sets if not found.
+     *
+     * @param slot      The slot
+     * @param itemStack The new ItemStack what should be displayed.
+     * @return true if the ItemStack was updated, false if set.
+     * @throws IllegalArgumentException if slot greater than 53 or slot greater than inventory size
+     */
+    public boolean updateOrSet(@Nonnegative int slot,
+                               @NotNull ItemStack itemStack) throws IllegalArgumentException {
+        return updateOrSet(slot, IntelligentItem.empty(itemStack));
+    }
+
+    /**
+     * Updates the ItemStack in the same place with a new ItemStack or sets if not found.
+     *
+     * @param slot      The slot
+     * @param intelligentItem The new ItemStack what should be displayed.
+     * @return true if the ItemStack was updated, false if set.
+     * @throws IllegalArgumentException if slot greater than 53 or slot greater than inventory size
+     */
+    public boolean updateOrSet(@Nonnegative int slot,
+                               @NotNull IntelligentItem intelligentItem) throws IllegalArgumentException {
+        if (get(slot).isPresent()) {
+            return update(slot, intelligentItem);
+        } else {
+            set(slot, intelligentItem);
+            return true;
+        }
+    }
+
+    /**
      * @param slot The slot
      * @return true if the slot is ignored.
      */
